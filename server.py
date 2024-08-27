9a81dc3282eea9311a5e8cf461398cb88d3434ab
trflorian <florian.trautweiler@hispeed.ch>
2024-08-27 22:25:06 +0200

    working server for firebase drop in replacement

--- server.py
+++ server.py
@@ -1,72 +1,63 @@
-from http.server import SimpleHTTPRequestHandler, HTTPServer
-from http.client import HTTPConnection
-import json
+from fastapi import FastAPI, Response
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+import json, uvicorn
+from asyncio import sleep
+from pydantic import BaseModel
+
+
+class Player(BaseModel):
+    id: int
+    position_x: float
+    position_y: float
+    color: str
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 players = {}
-streaming_clients: list[HTTPConnection] = []
 
-# create a simple server with the following two endpoints:
-# 1. /PLAYER_ID.json: PUT request -> sets the player data for the given player id
-# 2. /players.json: POST with text/event-stream -> starts the server-sent event stream for all players
+def wrap_players_in_firebase_json():
+    return json.dumps({
+        "path": "/",
+        "data": players
+    })
 
-
-# EVENT FOR SINGLE PLAYER
-# event: put
-# data: {"path": "/PLAYER001", "data": {"PLAYER001": "1", "x": 10, "y": 20}}
-
-# EVENT FOR ALL PLAYERS
-# event: put
-# data: {"path": "/", "data": {"PLAYER001": "1", "x": 10, "y": 20}, "PLAYER002": "2", "x": 30, "y": 40}
-
-def encode_single_player_event_data(player_id: str) -> str:
-    return "\n".join([
-        "event: put",
-        "data: " + json.dumps({
-            "path": f"/{player_id}",
-            "data": players[player_id]
-        })
-    ]) + "\n\n"
-
-def encode_all_players_event_data() -> str:
-    return "\n".join([
-        "event: put",
-        "data: " + json.dumps({
-            "path": "/",
-            "data": players
-        })
-    ]) + "\n\n"
-
-def handle_put_player_data(player_id: str, data: dict):
-    players[player_id] = data
-
-    event_data = encode_single_player_event_data(player_id)
-
-    # for client in streaming_clients:
-    #     client.send(event_data.encode())
+async def players_generator():
+    while True:
+        data = wrap_players_in_firebase_json()
+        yield f"event: put\ndata: {data}\n\n"
+        await sleep(0.02)
 
 
-class GameServerRequestHandler(SimpleHTTPRequestHandler):
-    def do_PUT(self):
-        player_id = self.path[1:-5]
-        data = json.loads(self.rfile.read().decode())
-        handle_put_player_data(player_id, data)
+@app.get("/players")
+async def root():
+    return StreamingResponse(players_generator(), media_type="text/event-stream")
 
-    def do_POST(self):
-        self.handle_post_players_streaming()
-        self.send_response(200)
+@app.put("/players/{player_id}")
+async def update_player(player_id: int, player: Player):
+    players[player_id] = {
+        "id": player_id,
+        "position_x": player.position_x,
+        "position_y": player.position_y,
+        "color": player.color,
+    }
+    return Response(status_code=200)
 
-    def handle_post_players_streaming(self):
-        self.send_response(200)
 
-        streaming_clients.append(self.wfile)
+@app.delete("/players/{player_id}")
+async def delete_player(player_id: int):
+    players.pop(player_id)
+    return Response(status_code=200)
 
-        event_data = encode_all_players_event_data()
-        self.wfile.write(event_data.encode())
-
-def run():
-    server = HTTPServer(("localhost", 8080), GameServerRequestHandler)
-    print(f"Server started at http://{server.server_address[0]}:{server.server_address[1]}")
-    server.serve_forever()
 
 if __name__ == "__main__":
-    run()
+    uvicorn.run(app, host="127.0.0.1", port=8000)
